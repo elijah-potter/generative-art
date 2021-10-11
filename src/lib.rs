67f7d19b240601bc8764a799b530d938b08500e3
Elijah Potter <elijah.sirius@protonmail.com>
2021-10-11 13:59:35 -0600

    Moved to wasm-pack template

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,19 @@
-mod sketch;
+mod utils;
 
-use image::{ImageEncoder, ImageFormat, png::PngEncoder};
-use sketch::Sketcher;
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::prelude::*;
 
-/// Create from file
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
-pub extern "C" fn sketch_file(file: &[u8], filename: &str, iterations: usize) -> Option<Vec<u8>> {
-    let format = match ImageFormat::from_path(filename) {
-        Ok(v) => v,
-        Err(_) => return None,
-    };
+extern {
+    fn alert(s: &str);
+}
 
-    let canvas = match image::load_from_memory_with_format(file, format) {
-        Ok(v) => v,
-        Err(_) => return None,
-    };
-
-    let canvas = canvas.into_rgba8();
-
-    let mut sketcher = Sketcher::new_preslav(canvas.to_owned(), iterations);
-    sketcher.max_edge_count = 6;
-
-    for _ in 0..iterations {
-        sketcher.draw_iter(&canvas);
-    }
-
-    let mut output = Vec::new();
-    let encoder = PngEncoder::new_with_quality(
-        &mut output,
-        image::png::CompressionType::Best,
-        image::png::FilterType::NoFilter,
-    );
-
-    let canvas = sketcher.get_canvas();
-    if encoder.write_image(canvas.as_flat_samples().samples, canvas.width(), canvas.height(), image::ColorType::Rgba8).is_err(){
-        return None;
-    };
-
-    Some(output)
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, generative-art!");
 }
