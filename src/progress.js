fe281c6768c80fda789cfa4f1145fa64b1819075
Elijah Potter <elijah.sirius@protonmail.com>
2021-10-11 21:21:39 -0600

    Removed bad stuff

--- src/progress.js
+++
@@ -1,9 +0,0 @@
-export function progress(i) {
-    const progress_bar = document.getElementById("progress");
-    console.log(i);
-    console.log(progress_bar);
-    progress_bar.value = i;
-    setTimeout(function () {
-        document.getElementById("pb").value=this;
-        if (this == max) console.log("Finished")
-      }.bind(i+1), i*16);}
