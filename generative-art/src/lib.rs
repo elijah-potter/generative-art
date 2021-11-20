a1ad900dd6b76cf48faf28fe3653d77875db8f45
Elijah Potter <elijah.sirius@protonmail.com>
2021-11-19 22:25:44 -0700

    Made Distributions public for ease-of-use

--- generative-art/src/lib.rs
+++ generative-art/src/lib.rs
@@ -1,8 +1,6 @@
 mod helpers;
 pub mod sketchers;
 
+pub use rand::distributions;
 pub use glam::Vec2;
 pub use image;
-// pub use sketchers::{
-//     CelestialSketcher, CelestialSketcherSettings, PreslavSketcher, PreslavSketcherSettings,
-// };
